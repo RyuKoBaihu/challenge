36a9772a6ac7c7ba3fdfb65479a3eb62e094fb03
RyuKoBaihu <yamamoto.araujo.matheus@gmail.com>
2023-05-07 16:43:13 -0300

    Final adjustments

--- src/main/java/br/com/challenge/ChallengeTest.java
+++ src/main/java/br/com/challenge/ChallengeTest.java
@@ -1,14 +1,13 @@
 package br.com.challenge;
-import br.com.challenge.metodoHashSet;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class ChallengeTest {
-    private static String frase = "Hello: hello my fellow candidate.";
+    private static String phrase = "Hello: hello my fellow candidate.";
 
     public static void main(String args[]) throws Exception {
-        String[] strArr = frase.split(" ");
+        String[] strArr = phrase.split(" ");
         ArrayList<String> list = new ArrayList<String>(Arrays.asList(strArr));
 
         List<String> editedList;
@@ -17,13 +16,13 @@ public class ChallengeTest {
                 .map(s -> s.replaceAll("\\W", ""))
                 .collect(Collectors.toList());
 
-        String primeiraPalavra = editedList.get(0);
+        String firstWord = editedList.get(0);
 
         Set set = new HashSet<>(editedList);
         if (set.size()==editedList.size()) {
             throw new Exception("There are no duplicate values");
         } else {
-            System.out.println(primeiraPalavra);
+            System.out.println(firstWord);
         }
 
     }
